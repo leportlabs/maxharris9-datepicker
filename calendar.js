dfc1c2ed8bf187018ffa89ddb73da3f2d564c887
Max Harris <harris.max@gmail.com>
2014-09-12 07:16:16 -0700

    Improved comments and names

--- calendar.js
+++ calendar.js
@@ -13,6 +13,7 @@ renderCalendar = function (calItems, pre, newRow, blank, filledDateFunc, post) {
 		if (0 === (i % 7)) {
 			output.push(newRow);
 		}
+
 		if (i < calItems.startingDay) {
 			output.push(blank);
 		}
@@ -29,24 +30,27 @@ renderCalendar = function (calItems, pre, newRow, blank, filledDateFunc, post) {
 
 Calendar = (function () {
 	return function (day, month, year, guid) {
-		var _day = day; // day could be used to mark the highlighted date - not helpful in calculating the calendar
+		var _day = day; // TODO: use day to mark the current day in the calendar HTML we emit
 		var _month = month;
 		var _year = year;
 		var _guid = guid;
 
 		var _getWeekDay = function () {
 			// inspired by http://stackoverflow.com/questions/18664548/print-a-12-month-calendar-with-only-input-being-the-year-using-c
-			var a = Math.floor((14 - _month) / 12); // Math.floor simulates integer divisions
-			var y = Math.floor(_year - a);
-			var m = Math.floor(_month + (12 * a) - 2);
 
-			var mysteriousTerm = Math.floor((31 * m) / 12);
-			var leapDayCount = Math.floor(y / 4); // number of leap days since March 1st, xx00?
-			var centuryOffset = Math.floor(y / 100); // century offset?
-			var fourHundredth = Math.floor(y / 400);
+			// Math.floor simulates integer divisions
+			var startDay = 1;
+			var offset = Math.floor((14 - _month) / 12); // for Jan and Feb, this is 1, otherwise it's 0
+			var offsetYear = Math.floor(_year - offset); // the year, offset by two months; so Jan and Feb 2000 -> 1999, while Mar 2000 -> 2000
+			var offsetMonth = Math.floor(_month + (12 * offset) - 2); // for Jan and Feb, this is 11 and 12, respectively
+
+			var offsetMonthShim = Math.floor((31 * offsetMonth) / 12); // 28, 31, 2, 5, 7, 10, 12, 15, 18, 20, 23, 25 - what does this do?
+			var leapDayCount = Math.floor(offsetYear / 4); // number of leap days since 4 AD
+			var centuryOffset = Math.floor(offsetYear / 100); // the century, offset by two months
+			var leapDayCountFirstDigit = Math.floor(offsetYear / 400); // is this valid past 4000 AD?
 
 			// return the day of the week for particular date. sunday: 0, monday: 1, tuesday: 2, ... saturday: 6
-			return (1 + y + leapDayCount - centuryOffset + fourHundredth + mysteriousTerm) % 7;
+			return (startDay + offsetYear + leapDayCount - centuryOffset + leapDayCountFirstDigit + offsetMonthShim) % 7;
 		};
 
 		var _fillCalendar = function () {
@@ -63,7 +67,13 @@ Calendar = (function () {
 
 		return {
 			renderHtmlCalendar: function () {
-				return renderCalendar(_calItems, "<tr>", "<tr>", "<td></td>", function (day) { return "<td class='day calendarDay' id='" + encodeId(day, _month,  _year, _guid) + "'>" + day + "</td>"; }, "<tr>");
+				return renderCalendar(
+					_calItems,
+					"<tr>",
+					"<tr>",
+					"<td></td>",
+					function (day) { return "<td class='day calendarDay' id='" + encodeId(day, _month,  _year, _guid) + "'>" + day + "</td>"; },
+					"<tr>");
 			},
 			__getTestState__: function () {
 				return _calItems;
