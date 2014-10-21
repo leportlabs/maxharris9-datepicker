e70d6346ff1bdceb01879bcddda05eaace337d1e
Chris Alexander <chris.alexander@mac.com>
2014-10-21 12:06:55 -0700

    Added styles and arrow component so that package can be better isolated

--- package.js
+++ package.js
@@ -6,9 +6,11 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('METEOR@0.9.1');
-	api.use(['standard-app-packages', 'tracker', 'templating', 'underscore', 'maxharris9:template-instance-utils', 'pfafman:font-awesome-4', 'mongo-livedata', 'mquandalle:stylus'], 'client');
+	api.use(['standard-app-packages', 'tracker', 'templating', 'underscore', 'maxharris9:template-instance-utils', 'pfafman:font-awesome-4', 'mongo-livedata', 'mquandalle:stylus@1.0.7'], 'client');
 
 	api.add_files('datepicker.styl', 'client');
+
+	api.add_files('arrow-icon.html', 'client');
 
 	api.add_files('maxharris9:datepicker.html', 'client');
 
